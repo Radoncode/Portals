ffc3b6bd1fd66124905b8da10c7d6c836233fab9
Radoncode <relbouni69@gmail.com>
2022-01-20 19:48:58 +0000

    When use Portals?

---
+++ src/Modal.js
@@ -0,0 +1,40 @@
+import { Component} from "react";
+import ReactDOM from 'react-dom';
+
+class Modal extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.popUpContainer = document.createElement('div');
+
+        
+    }
+
+    componentDidMount() {
+        document.body.appendChild(this.popUpContainer);
+    }
+
+    componentWillUnmount() {
+        document.body.removeChild(this.popUpContainer);
+    }
+    render(){
+        return ReactDOM.createPortal(
+            <div className="modal" onClick={this.props.close}>
+                <div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                         industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                           type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap
+                             into electronic typesetting, remaining essentially unchanged. 
+                    </p>
+                    <button>Fermer</button>
+                </div>
+            </div>,
+            this.popUpContainer
+        )
+    }
+}
+
+export default Modal;
